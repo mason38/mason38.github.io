2faa6340af12c66f179130e601bbb528fba351c8
Mason <2202413@spsd.sk.ca>
2023-10-20 21:18:34 -0600

    made the flag go to the highest point

--- Perlin Noise Project/sketch.js
+++ Perlin Noise Project/sketch.js
@@ -8,16 +8,19 @@ let noiseShiftMove = 0;
 let rectangleWidth = 2;
 let noiseShift = 0.01;
 let rectHeightTime = 20;
+let tallestX = 0; // these are variables for the highest point out of the rectangles
+let tallestY = 0; 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
+  generateTerrain(); // makes the terrain
 }
-
 
 function generateTerrain(){
   let x = 0;
+  
   rectHeightTime = noiseShiftMove;
+  highestY = height; // the highest y point is equal to the height of the canvas
   while(x<width){
     let rectHeight = noise(rectHeightTime);
     rectHeight = map(rectHeight,0,1,0,windowHeight);
@@ -26,25 +29,33 @@ function generateTerrain(){
     rectMode(CORNERS);
     rect(x, height, x+rectangleWidth, height - rectHeight);
     stroke(0);
-    x+=rectangleWidth;
+    
+    // check if this point is higher than the previous point
+    if (height - rectHeight < highestY) {
+      highestX = x;
+      highestY = height - rectHeight;
+    }
+    
+    x += rectangleWidth;
   }
 }
 
-function keyPressed(){
+function keyPressed(){ // when arrow key left or right is pressed, change rectangle width
   if(keyCode===LEFT_ARROW && rectangleWidth>1) rectangleWidth -= 1;
   else if(keyCode===RIGHT_ARROW) rectangleWidth += 1;
   background(255);
   generateTerrain();
+  drawFlag(20, 20);
 }
 
 function draw(){
   background(255);
   noiseShiftMove += 0.02;
   generateTerrain();
-  drawFlag(20,20);
+  drawFlag(20, 20);
 }
 
-function drawFlag(x,y){
-  line(x, y, x, y+30);
-  triangle(x,y+10,x,y,x+15,y+5);
+function drawFlag(x,y){ // draws the flag
+  line(highestX, highestY, highestX, highestY - 30); 
+  triangle(highestX, highestY - 20, highestX, highestY - 30, highestX + 15, highestY - 25); 
 }
