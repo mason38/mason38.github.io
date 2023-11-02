292b8ee7e788f48cb91e386eb75c8c0130ba1d37
Mason <2202413@spsd.sk.ca>
2023-11-01 19:36:31 -0600

    done gen art 3

--- Generative Art Design 3/sketch.js
+++ Generative Art Design 3/sketch.js
@@ -1,15 +1,27 @@
-// Project Title
-// Your Name
-// Date
+// Generative Art 
+// Mason L.
+// Nov. 1, 2023
 //
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
 
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+
+function setup(){
+  createCanvas(2000, 2000);
+  drawThings();
 }
 
-function draw() {
-  background(220);
+function drawThings(){
+  let x = random(0,2000);
+  let y = random(0,2000);
+  let x2 = random(0,2000);
+  let y2 = random(0,2000);
+  strokeWeight(1);
+  for(let i = 0; i < 100000; i++){
+    stroke(color(random(0,255)));
+    line(x,y,x2,y2);
+    x = random(0,2000);
+    y = random(0,2000);
+    x2 = random(0,2000);
+    y2 = random(0,2000);
+  }
 }
